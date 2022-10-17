320181ac12a1aa54803093bfbe0b7e8ac26ab49e
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:21:52 +0200

    Create 2-print_alphabet.c

---
+++ 2-print_alphabet.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-print_alphabet.c -o 2-print_alphabet
+siphotommy@alx-low_level_programming:~/0x01$ ./2-print_alphabet 
+abcdefghijklmnopqrstuvwxyz
+siphotommy@alx-low_level_programming:~/0x01$
