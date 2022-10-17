7db14dc78a425b510a2d7a80bc1442cb2e4d5531
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:35:00 +0200

    Create 6-print_numberz.c

---
+++ 6-print_numberz.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-print_numberz.c -o 6-print_numberz
+siphotommy@alx-low_level_programming:~/0x01$ ./6-print_numberz 
+0123456789
+siphotommy@alx-low_level_programming:~/0x01$ 
