580c13ed69937c6e17f455f9045babb5a5608a72
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 17:59:58 +0200

    Create 4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+siphotommy@alx-low_level_programming:~/c/0x00$ echo $?
+0
+siphotommy@alx-low_level_programming:~/c/0x00$
