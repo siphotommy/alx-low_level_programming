6c80e7613f95f3329be6406c122ec921a1a5908f
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:32:42 +0200

    Create 5-print_numbers.c

---
+++ 5-print_numbers.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_numbers
+siphotommy@alx-low_level_programming:~/0x01$ ./5-print_numbers 
+0123456789
+siphotommy@alx-low_level_programming:~/0x01$ :~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_numbers
