6ee557a9cb9647d1bc95d546f57a282871ab596d
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:37:14 +0200

    Create 7-print_tebahpla.c

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahpla
+siphotommy@alx-low_level_programming:~/0x01$ ./7-print_tebahpla
+zyxwvutsrqponmlkjihgfedcba
+siphotommy@alx-low_level_programming:~/0x01$
