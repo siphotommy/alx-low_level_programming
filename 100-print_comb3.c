6927b514644baae79c6e2b906d8f04416d3987f1
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:45:05 +0200

    Create 100-print_comb3.c

---
+++ 100-print_comb3.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-print_comb3.c -o 100-print_comb3
+siphotommy@alx-low_level_programming:~/0x01$ ./100-print_comb3
+01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89
+siphotommy@alx-low_level_programming:~/0x01$ 
