33fb1b186a0d8b67d32c266c02c9af709f911a8a
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 18:13:11 +0200

    Create 6-size.c

---
+++ 6-size.c
@@ -0,0 +1,17 @@
+siphotommy@alx-low_level_programming:~/c/0x00$ gcc 6-size.c -m32 -o size32 2> /tmp/32
+siphotommy@alx-low_level_programming:~/c/0x00$ gcc 6-size.c -m64 -o size64 2> /tmp/64
+julien@ubuntu:~/c/0x00$ ./size32
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+siphotommy@alx-low_level_programming:~/c/0x00$ ./size64
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 8 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+siphotommy@alx-low_level_programming:~/c/0x00$ echo $?
+0
+siphotommy@alx-low_level_programming:~/c/0x00$ 
