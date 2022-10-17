bc7fb95f27c7db51e6ba05d8e619421bac7493cd
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:39:33 +0200

    Create 8-print_base16.c

---
+++ 8-print_base16.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16
+siphotommy@alx-low_level_programming:~/0x01$ ./8-print_base16
+0123456789abcdef
+siphotommy@alx-low_level_programming:~/0x01$
