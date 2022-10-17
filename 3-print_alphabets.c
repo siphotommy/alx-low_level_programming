7098a3061a4da937779b25cea440ac26cfcb6b7a
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:24:24 +0200

    Create 3-print_alphabets.c

---
+++ 3-print_alphabets.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets
+siphotommy@alx-low_level_programming:~/0x01$ ./3-print_alphabets | cat -e
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$
+siphotommy@alx-low_level_programming:~/0x01$ 
