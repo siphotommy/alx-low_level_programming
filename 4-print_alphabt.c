de1d4dd3abfc1217f6df75892cbfa029b200f8ee
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:28:08 +0200

    Create 4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,5 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-print_alphabt.c -o 4-print_alphabt
+siphotommy@alx-low_level_programming:~/0x01$ ./4-print_alphabt 
+abcdfghijklmnoprstuvwxyz
+siphotommy@alx-low_level_programming:~/0x01$ ./4-print_alphabt | grep [eq]
+siphotommy@alx-low_level_programming:~/0x01$ 
