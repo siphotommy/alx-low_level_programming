5b66d836296682fdc034a5170df3fb7b681dc1ec
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:18:56 +0200

    Create 1-last_digit.c

---
+++ 1-last_digit.c
@@ -0,0 +1,28 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 629438752 is 2 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -748255693 is -3 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -1052791662 is -2 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -284805734 is -4 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -284805734 is -4 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 491506926 is 6 and is greater than 5
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 954249937 is 7 and is greater than 5
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 652334952 is 2 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -729688197 is -7 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of -729688197 is -7 and is less than 6 and not 0
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 45528266 is 6 and is greater than 5
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 45528266 is 6 and is greater than 5
+siphotommy@alx-low_level_programming:~/0x01$ ./1-last_digit 
+Last digit of 809065140 is 0 and is 0
+siphotommy@alx-low_level_programming:~/0x01$
