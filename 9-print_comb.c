2005cefe6dd5418d31eadc2464c5039583e8e173
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 21:42:35 +0200

    Create 9-print_comb.c

---
+++ 9-print_comb.c
@@ -0,0 +1,4 @@
+siphotommy@alx-low_level_programming:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-print_comb.c -o 9-print_comb
+siphotommy@alx-low_level_programming:~/0x01$ ./9-print_comb | cat -e
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
+siphotommy@alx-low_level_programming:~/0x01$ 
