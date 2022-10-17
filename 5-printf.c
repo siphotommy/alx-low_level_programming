4536846f4e6304d0fc5fb4b21d811b681533a3d9
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-17 18:07:30 +0200

    Create 5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,3 @@
+siphotommy@alx-low_level_programming:~/c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
+siphotommy@alx-low_level_programming:~/c/0x00$ ./a.out 
+siphotommy@alx-low_level_programming:~/c/0x00$ echo $?
