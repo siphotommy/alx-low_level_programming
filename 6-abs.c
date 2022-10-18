ddff1afdc4b1aefccaebfd0bfc0e9d421c403c90
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-18 10:16:53 +0200

    Create 6-abs.c

---
+++ 6-abs.c
@@ -0,0 +1,30 @@
+Siphotommy@alx-low_level_programming:~/0x02$ cat 6-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
+}
+Siphotommy@alx-low_level_programming:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-abs.c -o 6-abs
+Siphotommy@alx-low_level_programming:~/0x02$ ./6-abs 
+1
+0
+1
+98
+Siphotommy@alx-low_level_programming:~/0x02$ 
