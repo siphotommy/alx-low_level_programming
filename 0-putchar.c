06459afdb2355fdbf81d991a544c605a5742e38c
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-18 10:01:52 +0200

    Create 0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,4 @@
+Siphotommy@alx-low_level_programming:~/0x02$  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+Siphotommy@alx-low_level_programming:~/0x02$ ./0-putchar 
+_putchar
+Siphotommy@alx-low_level_programming:~/0x02$ 
