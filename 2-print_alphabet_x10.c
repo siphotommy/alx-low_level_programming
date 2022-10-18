9bfd62f6651ffaffdc1f1c1bf1cbe7dc2e1ed626
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-18 10:09:32 +0200

    Create 2-print_alphabet_x10.c

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+Siphotommy@alx-low_level_programming:~/0x02$ cat 2-main.c
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
+Siphotommy@alx-low_level_programming:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10
+Siphotommy@alx-low_level_programming:~/0x02$ ./2-alphabet_x10 
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyz
+Siphotommy@alx-low_level_programming:~/0x02$ 
