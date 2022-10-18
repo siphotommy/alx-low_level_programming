3c7a59f298f0220050fa56e6b959f45c0114380b
siphotommy <114058058+siphotommy@users.noreply.github.com>
2022-10-18 10:11:21 +0200

    Create 3-islower.c

---
+++ 3-islower.c
@@ -0,0 +1,25 @@
+Siphotommy@alx-low_level_programming:~/0x02$ cat 3-main.c 
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+Siphotommy@alx-low_level_programming:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-islower.c -o 3-islower
+Siphotommy@alx-low_level_programming:~/0x02$ ./3-islower 
+011
+Siphotommy@alx-low_level_programming:~/0x02$ 
